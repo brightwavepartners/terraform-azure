286f09ca4c99d70e00b11cdde978e1bcf8e79641
Michael Gressman <michael.gressman@brightwavepartners.com>
2021-12-17 17:32:03 -0600

    uncomment the rest of the code

--- modules/resource-group/main.tf
+++ modules/resource-group/main.tf
@@ -8,27 +8,27 @@ module "globals" {
   tenant      = var.tenant
 }
 
-# # # resource group
-# # resource "azurerm_resource_group" "resource_group" {
-# #   location = var.location
-# #   name     = lower(module.globals.resource_base_name_long)
-# #   tags     = var.tags
-# # }
+# resource group
+resource "azurerm_resource_group" "resource_group" {
+  location = var.location
+  name     = lower(module.globals.resource_base_name_long)
+  tags     = var.tags
+}
 
-# # # add contributors to the resource group - TODO: is it possible to look up the principal id by name so we only have to specify names instead of identifiers
-# # resource "azurerm_role_assignment" "contributor" {
-# #   for_each = { for contributor in var.contributors : contributor.name => contributor }
+# add contributors to the resource group - TODO: is it possible to look up the principal id by name so we only have to specify names instead of identifiers
+resource "azurerm_role_assignment" "contributor" {
+  for_each = { for contributor in var.contributors : contributor.name => contributor }
 
-# #   scope                = azurerm_resource_group.resource_group.id
-# #   role_definition_name = "Contributor"
-# #   principal_id         = each.value.object_id
-# # }
+  scope                = azurerm_resource_group.resource_group.id
+  role_definition_name = "Contributor"
+  principal_id         = each.value.object_id
+}
 
-# # # add readers to the resource group - TODO: is it possible to look up the principal id by name so we only have to specify names instead of identifiers
-# # resource "azurerm_role_assignment" "reader" {
-# #   for_each = { for reader in var.readers : reader.name => reader }
+# add readers to the resource group - TODO: is it possible to look up the principal id by name so we only have to specify names instead of identifiers
+resource "azurerm_role_assignment" "reader" {
+  for_each = { for reader in var.readers : reader.name => reader }
 
-# #   scope                = azurerm_resource_group.resource_group.id
-# #   role_definition_name = "Reader"
-# #   principal_id         = each.value.object_id
-# # }
+  scope                = azurerm_resource_group.resource_group.id
+  role_definition_name = "Reader"
+  principal_id         = each.value.object_id
+}
