6c2cb0b0fba376f60ea8f01b9d3d194584e0cd0a
Michael Gressman <michael.gressman@brightwavepartners.com>
2022-02-19 18:30:44 -0600

    move resource group examples

--- samples/vnet-to-vnet-vpn-gateway/main.tf
+++ samples/vnet-to-vnet-vpn-gateway/main.tf
@@ -23,22 +23,3 @@ module "southcentralus" {
     tenant = local.tenant
     vpn_gateway_shared_key = local.vpn_gateway_shared_key
 }
-
-# most of the infrastructure and networking components for each region
-# are created in the region module, but the virtual network gateway connections
-# between the two regions can't be part of the region module because that would create
-# a circular dependency between the two region modules. therefore, the connections
-# are created here after both region modules are already created.
-
-
-# # resource "azurerm_virtual_network_gateway_connection" "southcentralus_to_northcentralus" {
-# #   name                = lower("${module.globals.resource_base_name_long}-${module.globals.role_names.network}-${module.globals.object_type_names.virtual_network}")
-# #   location            = module.southcentralus.location
-# #   resource_group_name = module.southcentralus.resource_group_name
-
-# #   type                            = "Vnet2Vnet"
-# #   virtual_network_gateway_id      = module.southcentralus.virtual_network_gateway.id
-# #   peer_virtual_network_gateway_id = module.northcentralus.virtual_network_gateway.id
-
-# #   shared_key = local.vpn_gateway_shared_key
-# # }
