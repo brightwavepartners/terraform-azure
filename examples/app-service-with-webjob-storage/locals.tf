6a5d6d0023fd1c82814b138cf8cefc5403f7f5e6
Michael Gressman <michael.gressman@brightwavepartners.com>
2023-03-21 09:27:47 -0500

    move webjobs storage account configuration to locals file

--- examples/app-service-with-webjob-storage/locals.tf
+++ examples/app-service-with-webjob-storage/locals.tf
@@ -1,6 +1,18 @@
 locals {
   app_service = {
     role = "appone"
+    # the presence of this webjobs_storage block signals we want
+    # this app service to host webjobs and so we need a storage
+    # account. if the app service is not going to host webjobs,
+    # just remove the webjobs_storage block. there is no enable/disable
+    # flag for the webjobs_storage and it is turned on and off
+    # simply through the existence of this block.
+    webjobs_storage = {
+      alert_settings = [],
+      vnet_integration = {
+        enabled = false
+      }
+    }
   }
   app_service_plan = {
     os_type  = "Windows"
