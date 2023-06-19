84227d7f71b6e132405fd4cbfc25d54b1fafd23e
Michael Gressman <michael.gressman@brightwavepartners.com>
2023-06-18 21:41:54 -0500

    add resource_group_name variable to queue module of service_bus module

--- modules/service_bus/queue/variables.tf
+++ modules/service_bus/queue/variables.tf
@@ -11,3 +11,8 @@ variable "queue" {
     }
   )
 }
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which the service bus is provisioned."
+}
