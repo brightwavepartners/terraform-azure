5330812cbe34e3fb04d9da852698b1fabb8928df
Michael Gressman <michael.gressman@brightwavepartners.com>
2023-07-24 21:40:05 -0500

    working on front door endpoints

--- modules/front_door/maint.tf
+++ modules/front_door/maint.tf
@@ -1,3 +1,15 @@
+locals {
+  v = flatten(
+    [
+      for endpoint in var.endpoints : [
+        for route in endpoint.routes : {
+          route_name = route.name
+        }
+      ]
+    ]
+  )
+}
+
 # global naming conventions and resources
 module "globals" {
   source = "../globals"
@@ -26,16 +38,13 @@ resource "azurerm_cdn_frontdoor_profile" "front_door_profile" {
 resource "azurerm_cdn_frontdoor_endpoint" "endpoints" {
   for_each = { for endpoint in var.endpoints : endpoint.name => endpoint }
 
-  name = each.value.name
+  name                     = each.value.name
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.front_door_profile.id
 }
 
 resource "azurerm_cdn_frontdoor_origin_group" "origin_groups" {
   for_each = {
-    for endpoint in var.endpoints : endpoint.name =>
-    [
-      for route in endpoint.routes : route.origin_group
-    ]
+    for x in local.v : x.route_name => x
   }
 
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.front_door_profile.id
@@ -43,5 +52,5 @@ resource "azurerm_cdn_frontdoor_origin_group" "origin_groups" {
   load_balancing {
   }
 
-  name = each.value.name
+  name = each.value.route_name
 }
