c2b7da8bc5ded1358c0fac8df39f0cf401a3a960
mgressman <michael.gressman@brightwavepartners.com>
2025-01-21 21:41:28 -0600

    updated app_registration module example

--- examples/app-registration/locals.tf
+++ examples/app-registration/locals.tf
@@ -1,13 +1,23 @@
 locals {
-    app_registration_server = {
-        api = {
-            identifier_uri = "api://server"
-        }
-        name = "Server"
-        web = {
-            redirect_uris = [
-                "https://oauth.pstmn.io/v1/callback"                
-            ]
-        }
+  app_registration_server = {
+    api = {
+      identifier_uri = "api://server"
     }
+    name = "Server"
+    web = {
+      "implicit_grant" : {
+        "issue_access_tokens" : true,
+        "issue_id_tokens" : true
+      },
+      redirect_uris = [
+        "https://oauth.pstmn.io/v1/callback"
+      ]
+    }
+  }
+  application = "appregistration"
+  environment = "dev"
+  key_vault = {
+    sku = "standard"
+  }
+  location = "northcentralus"
 }
--- examples/app-registration/main.tf
+++ examples/app-registration/main.tf
@@ -1,6 +1,47 @@
+# global naming conventions and resources
+module "globals" {
+  source = "../../modules/globals"
+
+  application = local.application
+  environment = local.environment
+  location    = local.location
+  tenant      = var.tenant
+}
+
+# resource group
+module "resource_group" {
+  source = "../../modules/resource_group"
+
+  application  = local.application
+  contributors = []
+  environment  = local.environment
+  location     = local.location
+  readers      = []
+  tags         = {}
+  tenant       = var.tenant
+}
+
+module "key_vault" {
+  source = "../../modules/key_vault"
+
+  application         = local.application
+  environment         = local.environment
+  location            = local.location
+  resource_group_name = module.resource_group.name
+  sku                 = local.key_vault.sku
+  tags                = {}
+  tenant              = var.tenant
+}
+
 # app registration - server
 module "app_registration_server" {
   source = "../../modules/app_registration"
 
-  name = local.app_registration_server.name
+  api = {
+    identifier_uri = local.app_registration_server.api.identifier_uri
+    scopes = []
+  }
+  key_vault_id = module.key_vault.id
+  name         = local.app_registration_server.name
+  web          = local.app_registration_server.web
 }
